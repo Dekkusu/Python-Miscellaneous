76bed76a292d506667d79489071b9af46fd6789f
Dekkusu <dexlutheroguimas@gmail.com>
2023-03-22 02:48:47 +0800

    updated CSVWriter

--- CsvWriter.py
+++ CsvWriter.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 import time
 import os
+import numpy as np
 
 hour_now = time.strftime("%H") + ":00"
 
@@ -40,13 +41,17 @@ def create_temp_csv(csv_filedata):
     csv_filename = directory+"temp.csv"
     list_data_count_keys = list(csv_filedata.keys())
     list_data_count_values = list(csv_filedata.values())
+    initial_value = [0, hour_now, 0, 0, 0, 0]
     vehicle_count_to_add = []
     try:
-        reader = pd.read_csv(csv_file)
+        reader = pd.read_csv(csv_filename)
         try:
-            # for line_count in range(len(reader.index)):
-            # previous vehicle count data
-            if (reader.loc[0, list_data_count_keys[2:6]] > 0).any():
+            if list_data_count_values[2:6] == list(reader.loc[1, list_data_count_keys[2:6]]):
+                reader.loc[2, list_data_count_keys[2:6]
+                           ] = initial_value[2:6]
+                reader.to_csv(csv_filename, index=False)
+                print("free pass")
+            elif (reader.loc[0, list_data_count_keys[2:6]] > 0).any():
                 '''Have atleast 1 element/data greater than 0'''
                 reader.loc[1, list_data_count_keys] = list_data_count_values    # new vehicle count data
                 vehicle_count_to_add.extend(np.subtract(
@@ -54,22 +59,23 @@ def create_temp_csv(csv_filedata):
                 reader.loc[2, list_data_count_keys[2:6]] = vehicle_count_to_add
                 # new value of previous data
                 reader.loc[0, list_data_count_keys[2:6]] = vehicle_count_to_add
-                reader.to_csv(csv_file, index=False)
-                print(f"Added new line {csv_file}")
+                reader.to_csv(csv_filename, index=False)
+                print(f"Added new line {csv_filename}")
             else:
                 '''If whole row is 0 or empty'''
-                # reader.loc[0, list_data_count_keys[2:6]] = vehicle_count_to_add
-                # reader.to_csv(csv_file, index=False)
+                reader.loc[1, list_data_count_keys[2:6]
+                           ] = reader.loc[1, list_data_count_keys[2:6]]
+                reader.loc[2, list_data_count_keys[2:6]
+                           ] = reader.loc[1, list_data_count_keys[2:6]]
+                reader.to_csv(csv_filename, index=False)
                 print("zero")
         except Exception as e:
             print(e)
     except:
-        initial_value = [0, hour_now, 0, 0, 0, 0]
         with open(csv_filename, "w", newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(list_data_count_keys)
             writer.writerow(list_data_count_values)
-            # writer.writerow(initial_value)
             writer.writerow(list_data_count_values)
             writer.writerow(list_data_count_values)
 
@@ -77,24 +83,34 @@ def to_csv_leg_data(csv_filename, csv_filedata):
     csv_file = directory+csv_filename
     list_data_count_keys = list(csv_filedata.keys())
     list_data_count_values = list(csv_filedata.values())
+    final_vehicle_count_to_add = []
     try:
         reader = pd.read_csv(csv_file)
         try:
             for line_count in range(len(reader.index)):
                 if reader.loc[line_count, "Time"] == list_data_count_values[1]:
-                    reader.loc[line_count, list_data_count_keys[2:6]] = list_data_count_values[2:6]
-                    # reader.loc[line_count, "Total Vehicles"] = sum(reader.loc[line_count, list_data_count_keys[2:6]])      #change x in [2:x] if new csv header is added
-                    reader.to_csv(csv_file, index=False)
-                    print(f"Successfully Updated {csv_file}")
-                    break
+                    if (reader.loc[line_count, list_data_count_keys[2:6]]>0).any():
+                        '''Have atleast 1 element/data greater than 0'''
+                        create_temp_csv(csv_filedata)
+                        temp_reader = pd.read_csv(directory+"temp.csv")
+                        final_vehicle_count_to_add.extend(temp_reader.loc[2, list_data_count_keys[2:6]])
+                        #print(final_vehicle_count_to_add)
+                        reader.loc[line_count, list_data_count_keys[2:6]
+                                   ] += final_vehicle_count_to_add
+                        reader.to_csv(csv_file, index=False)
+                        #print("not Zero")
+                        break
+                    else:
+                        '''If whole row is 0 or empty'''
+                        #print("zero")
+                        reader.loc[line_count, list_data_count_keys[2:6]] += list_data_count_values[2:6]
+                        reader.to_csv(csv_file, index=False)
+                        print(f"Successfully Updated {csv_file}")
+                        break
                 if line_count == range(len(reader.index))[-1] and reader.loc[line_count, "Time"] != list_data_count_values[1]:
-                    # reader.to_csv(csv_file, mode='a', header=False)
-                    with open(csv_file, "a", newline='') as csvfile:
-                        writer = csv.writer(csvfile)
-                        writer.writerow(list_data_count_values)
-                        print(f"Successfully Added New Line in {csv_file}")
-        except:
-            print(f"Error Updating {csv_file}")  # change print content
+                    print(f"Time Invalid")
+        except Exception as e: print(e)
+            # print(f"Error Updating {csv_file}")  # change print content
     except:
         create_empty_lane_csv(csv_file)
         to_csv_leg_data(csv_filename, csv_filedata)
@@ -113,7 +129,6 @@ def to_csv_leg_data(csv_filename, csv_filedata):
         reader.to_csv(csv_file, index=False)
     except:
         print(f"Error Updating Total {csv_file}")  # change print content
-
 
 def to_csv_vehicle_class_breakdown(csv_filename, csv_filedata):
     csv_file = directory+csv_filename
@@ -144,6 +159,7 @@ def to_csv_vehicle_class_breakdown(csv_filename, csv_filedata):
             writer.writerow(list_vehicle_class_count_keys)
             writer.writerow(list_vehicle_class_count_values)
 
+
 # Sample data run
 
 
@@ -160,11 +176,12 @@ vehicle_count_data = {
 lane_data = {
     "Date" : 0.0,
     "Time" : hour_now,
-    "Camera 1" : 5,
+    "Camera 1" : 2,
     "Camera 2" : 5,
-    "Camera 3" : 5,
-    "Camera 4" : 5,
+    "Camera 3" : 10,
+    "Camera 4" : 11,
 }
 
-to_csv_vehicle_class_breakdown(class_breakdown_filename, vehicle_count_data)
+# to_csv_vehicle_class_breakdown(class_breakdown_filename, vehicle_count_data)
 to_csv_leg_data(leg_filename, lane_data)
+# create_temp_csv(lane_data)
